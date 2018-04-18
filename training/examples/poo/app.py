2ac38bd59ad985c75e5bf5ce1500e0ff5185b5ad
Juan Sosa <user.nuage@gmail.com>
2018-04-18 11:45:24 -0500

    Add Empleado to POO Training

--- training/examples/poo/app.py
+++ training/examples/poo/app.py
@@ -1,5 +1,6 @@
 from persona import Persona
 from cliente import Cliente
+from empleado import Empleado
 
 
 personas = [Persona("Juan", 30),
@@ -9,9 +10,16 @@ personas = [Persona("Juan", 30),
 for persona in personas:
     print(persona)
 
+print()
+
 juan = Cliente("Juan", 40, 1_000_000)
 juan.hablar("Hola")
 print("Es Persona: {}".format(isinstance(juan, Persona)))
 print("Es Cliente: {}".format(isinstance(juan, Cliente)))
 juan.comprar("telefono", 1_000_000)
 print(juan.get_dinero())
+
+print()
+
+pepe = Empleado("Pepe", 21, "Vendedor")
+pepe.atender(juan)
---
+++ training/examples/poo/empleado.py
@@ -0,0 +1,33 @@
+from persona import Persona
+from cliente import Cliente
+
+
+class Empleado(Persona):
+    def __init__(self, nombre, edad, cargo):
+        super().__init__(nombre, edad)
+        self.set_cargo(cargo)
+
+    def set_cargo(self, cargo):
+        if(isinstance(cargo, str)):
+            if(len(cargo) >= 3):
+                self.__role = cargo
+            else:
+                print("El cargo debe tener al menos 3 caracteres")
+        else:
+            print("El cargo debe ser un valor String")
+
+    def get_cargo(self):
+        return self.__role
+
+    def atender(self, cliente):
+        if(isinstance(cliente, Cliente)):
+            print('{} esta atendiendo al cliente {}'.format(
+                self.get_nombre(), cliente.get_nombre()))
+        else:
+            print("Solo podemos atender a clientes")
+
+
+if __name__ == '__main__':
+    print('Loading Cliente')
+else:
+    print('Importing Cliente')
